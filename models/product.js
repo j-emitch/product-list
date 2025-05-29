b165524ff19e0842639a1af59f7fd38c1fc45698
Joseph Mitchell <joseph.e.mitchell17@gmail.com>
2025-05-28 20:15:36 -0400

    add explicit ReviewSchema subdoc

--- models/product.js
+++ models/product.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const ReviewSchema = new Schema({
+  username: String,
+  text: String,
+});
+
 const ProductSchema = new Schema({
   category: String,
   name: String,
   price: Number,
   image: String,
-  reviews: [
-    {
-      username: String,
-      rating: Number,
-      comment: String,
-    },
-  ],
+  reviews: [ ReviewSchema],
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
